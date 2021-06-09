f729f218b269b67a700d87024d59fc845fb50028
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-09 14:55:26 +0200

    correction ajout HTML

--- exercice1.6.php
+++ exercice1.6.php
@@ -1,4 +1,16 @@
 <?php
-$name = "Frodo Baggins";
-echo 'Bonjour ' .$name. ', comment vas-tu?';
+    $name = 'Frodo Baggins' ;
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 1.6</title>
+</head>
+<body>
+    <h1>Exercice 6 Partie 1 : Les variables PHP</h1>
+    <p><?= 'Bonjour ' .$name. ', comment vas-tu?';?></p>
+</body>
+</html>
